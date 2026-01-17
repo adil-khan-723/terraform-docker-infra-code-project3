48e2171007ec23e7133698df28ee26ee58cfb8bd
oggy3682 <adilk81054@gmail.com>
2026-01-17 23:14:11 +0530

    added aws installation in the user data

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -110,4 +110,10 @@ sleep 10
 systemctl --no-pager status jenkins
 docker ps
 
+#AWS INSTALLATION
+apt update -y
+apt install -y unzip curl
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+
 echo "===== USER DATA COMPLETE ====="
