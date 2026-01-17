5c07b7e5cc987b34aaaf85d8048736db7104eb23
oggy3682 <adilk81054@gmail.com>
2026-01-17 13:24:58 +0530

    made changes to the image tags

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "environment" {
   type        = string
   description = "type of environment (dev/prod)"
 }
+
+variable "backend_image_tag" {
+  type        = string
+  description = "Docker image tag for backend (from CI)"
+}
+
+variable "frontend_image_tag" {
+  type        = string
+  description = "Docker image tag for frontend (from CI)"
+}
